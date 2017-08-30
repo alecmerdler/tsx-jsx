ddde4c17c39d5649941c5e214d059e72697869d4
Alec Merdler <alec.merdler@coreos.com>
2017-08-30 13:11:02 -0700

    adding @augments JSDoc does not work

--- hello.jsx
+++ hello.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+/** @augments {React.Component<object, object>} */
 export class Hello extends React.Component {
   render() {
     return <div>{this.props.baz}</div>;
